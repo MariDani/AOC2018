2cd8e803a357b7abc09dbb9e913a8e993bb55b77
Mariana Danielova <danielova.mariana@gmail.com>
2019-01-07 13:15:07 +0100

    Day 7 part 2

--- solutions/day7.js
+++ solutions/day7.js
@@ -14,7 +14,7 @@ module.exports = {
 
             // must check conditions before taking first element
             [_parent, queue] = getFirstQueueElem(results, tree, queue);
-            
+
             results = results + _parent;
 
             // find children of the first element/parent and push them to queue
@@ -30,12 +30,90 @@ module.exports = {
 
 
     getSecondStar: (i) => {
+        // https://hackernoon.com/breadth-first-search-in-javascript-e655cd824fa4
+        const tree = getCoordinates(i);
 
+        let queue = findRoots(tree);
+        let results = "";
+
+        let workers = createWorkers(5);        
+        let second = -1;
+        let workFinsihed = false;
+
+        while (!workFinsihed) {
+            second++;
+
+            // update remaining step counts for worker's assignments
+            workers.forEach(w => {
+                if (w.stepName) {
+                    w.stepCount--;
+                }
+                if (w.stepCount === 0) {
+                    results = results + w.stepName;
+
+                    // find children of the first element/parent and push them to queue
+                    let children = findChildren(w.stepName, tree);
+                    children.forEach(child => {
+                        if (queue.indexOf(child) < 0) {
+                            queue.push(child);
+                        }
+                    });
+
+                    w.stepCount = -1;
+                    w.stepName = undefined;
+                }
+            });
+
+            // sort queue
+            queue.sort();
+
+            workers.forEach(w => {
+                if (!w.stepName && queue.length > 0) {
+                    // must check conditions before assigning step
+                    [w.stepName, queue] = getFirstQueueElem(results, tree, queue);
+                    // assing number of steps
+                    if (w.stepName) {
+                        w.stepCount = getStepCount(w.stepName);
+                    }
+                }
+            });
+
+            workFinsihed = isWorkFinished(workers);
+
+        }
+        return second;
     }
 };
 
+
+function createWorkers(workersCount){
+    let workers = new Array();
+    while (workersCount > 0) {
+        workers.push({ stepName: undefined, stepCount: -1 });
+        workersCount--;
+    }
+    return workers;
+}
+
+
+function isWorkFinished(workers) {
+    let workFinsihed = true;
+    workers.forEach(w => {
+        if (w.stepName) workFinsihed = false;
+    })
+    return workFinsihed;
+}
+
+
+function getStepCount(letter) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    return alphabet.indexOf(letter.toLowerCase()) + 1 + 60;
+}
+
+
 function getFirstQueueElem(results, tree, queue) {
     let parentFound = false;
+    let hasParent = false;
     let parent;
     let parentIdx = 0
 
@@ -45,8 +123,13 @@ function getFirstQueueElem(results, tree, queue) {
         let conditionsFulfilled = true;
         let nodeIdx = 0;
         while (conditionsFulfilled && nodeIdx < tree.length) {
-            if (tree[nodeIdx].edge[1] === parent && results.indexOf(tree[nodeIdx].edge[0]) < 0) {
-                conditionsFulfilled = false;
+            if (tree[nodeIdx].edge[1] === parent) {
+
+                if (results.indexOf(tree[nodeIdx].edge[0]) < 0) {
+                    conditionsFulfilled = false;
+                }
+
+                hasParent = true;
             }
             nodeIdx++;
         }
@@ -54,8 +137,14 @@ function getFirstQueueElem(results, tree, queue) {
 
         parentIdx++;
     }
-    queue.splice(queue.indexOf(parent), 1);
-    return [parent, queue];
+    if (parentFound || !hasParent) {
+        queue.splice(queue.indexOf(parent), 1);
+        return [parent, queue];
+    }
+    else {
+        return [undefined, queue];
+    }
+
 }
 
 function findChildren(parent, tree) {
@@ -74,11 +163,11 @@ function findRoots(tree) {
     tree.forEach(_parent => {
         let foundParent = true;
         tree.forEach(_node => {
-            if (_parent.vertex === _node.edge[1]){
+            if (_parent.vertex === _node.edge[1]) {
                 foundParent = false;
             }
         });
-        if (foundParent && root.indexOf(_parent.vertex) < 0){
+        if (foundParent && root.indexOf(_parent.vertex) < 0) {
             root.push(_parent.vertex);
         }
     });
