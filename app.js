6a30bf5adcb02685cfee9a632ea23f0e6c2c1ea0
Mariana Danielova <danielova.mariana@gmail.com>
2019-01-09 15:59:58 +0100

    Day 9 part 1

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 const fs = require("fs");
 
-// const argv = require("yargs") 
-//     .usage("Get AOC2018 solutions")
-//     .help("help").alias("help", "h")
-//     .options({
-//         day: { description: "Day number", requiresArg: true, required: true },
-//         function: { description: "Function name (for ex. getFirstStar, getSecondStar)", requiresArg: true, required: true }
-//     })
-//     .argv;
-
-// const fPath = require(`./solutions/day${argv.day}.js`);
-// const inputPath = `./input/day${argv.day}.txt`;
-// const fName = argv.function;
-
-const fPath = require("./solutions/day7.js");
-const inputPath = "./input/day7.txt";
+const fPath = require("./solutions/day9.js");
+const inputPath = "./input/day9.txt";
 const fName = "getFirstStar";
 
 let promisedData = readData(inputPath);
---
+++ solutions/day9.js
@@ -0,0 +1,54 @@
+module.exports = {
+
+    getFirstStar: (i) => {
+
+        [playerTotal, marbleTotal] = getPlayersMarbles(i);
+
+        let players = new Array(playerTotal);
+        players.fill(0);
+        let marbleCircle = [0];
+
+        let currentPlayer = 0;
+        let currentMarbleIdx = 0;
+        let marbleNumber = 1;
+
+        while (marbleNumber <= marbleTotal) {
+
+            if (marbleNumber % 23 === 0) {
+                players[currentPlayer] = players[currentPlayer] + marbleNumber;
+                currentMarbleIdx = currentMarbleIdx - 7 < 0 ? currentMarbleIdx - 7 + marbleCircle.length : currentMarbleIdx - 7;
+                players[currentPlayer] = players[currentPlayer] + marbleCircle[currentMarbleIdx];
+                marbleCircle.splice(currentMarbleIdx, 1);
+
+            }
+            else {
+                currentMarbleIdx = currentMarbleIdx + 2 > marbleCircle.length ? currentMarbleIdx + 2 - marbleCircle.length : currentMarbleIdx + 2
+                marbleCircle.splice(currentMarbleIdx, 0, marbleNumber);
+            }
+
+            marbleNumber++;
+            currentPlayer++;
+            if (currentPlayer >= playerTotal) {
+                currentPlayer = 0;
+            }
+        }
+
+        return Math.max(...players);
+
+    },
+
+
+    getSecondStar: (i) => {
+
+    }
+};
+
+
+
+function getPlayersMarbles(i) {
+
+    const regex = /(\d*)\splayers; last marble is worth\s(\d*)/s;
+    const playersMarbles = regex.exec(i);
+
+    return [parseFloat(playersMarbles[1]), parseFloat(playersMarbles[2])];
+}
